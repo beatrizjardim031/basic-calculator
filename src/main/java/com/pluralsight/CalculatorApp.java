f07ca6859a2ee53f50b90594c7ffd836abed449c
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-10 10:57:16 -0500

    Fix: Changed into to double so the division would work properly

--- src/main/java/com/pluralsight/CalculatorApp.java
+++ src/main/java/com/pluralsight/CalculatorApp.java
@@ -9,22 +9,31 @@ public class CalculatorApp {
         System.out.println("Welcome to Bea's first calculator!\n");
 //  Enter numbers
         System.out.print("Enter number 1: ");
-        int num1 = input.nextInt();
+        double num1 = input.nextDouble();
 
         System.out.print("Enter number 2: ");
-        int num2 = input.nextInt();
+        double num2 = input.nextDouble();
 
 /*  I used printf so I could add a space between the number input and possible calculator,
     I also used \n so I could print the options on a new line each time */
 
-        System.out.printf("%n Possible calculations: \n (A)dd \n (S)ubtract \n (M)ultiply \n (D)ivide %n");
+        System.out.printf("%n Possible calculations: \n (1) Add \n (2) Subtract \n (3) Multiply \n (4) Divide %n");
         System.out.println("Please select your option: ");
         input.nextLine();
-        String option = input.nextLine();
+        int option = input.nextInt();
 
-        System.out.println("Preparing to do Math✨✨✨");
+        System.out.printf("Preparing to do Math ✨✨✨%n");
 
+        if (option == 1){
+            System.out.println("Here is your result: " + (num1 + num2));
 
+        } else if (option == 2) {
+            System.out.println("Here is your result: " + (num1 - num2));
+        } else if (option == 3) {
+            System.out.println("Here is your result: " + (num1 * num2));
+        } else {
+            System.out.printf("Here is your result: %.2f", (num1 / num2));
+        }
 
     }
 }
